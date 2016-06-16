7bb30fc1c1500f9fd43693aca755aef3eaa03838
Aleks Merser <enderman.com290@gmail.com>
2016-06-16 23:47:01 +0400

    Движочек на DreamAPI
    Движочек на DreamAPI

---
+++ main.py
@@ -0,0 +1,8 @@
+print("There is good!")
+#print(abs(-99))
+shop = str(input("Please enter you dream item: "))
+print(shop)
+shopp = str(input("You dream price:"))
+print(shopp)
+money = str(input("You have money now?: "))
+print(money)
