64237b5c5cbc5ecad601af761280b61c6d84b07d
Aleks Merser <enderman.com290@gmail.com>
2016-06-17 13:39:06 +0400

    DreamAPI.v2
    DREAM API NOW IN V2

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 print("There is good!")
 #print(abs(-99))
+dreamitem = ["balik","lol"]
 shop = str(input("Please enter you dream item: "))
 print(shop)
+dreamitem.append(shop)
+dreamitemprice = [120,134]
 shopp = int(input("You dream price:"))
 print(shopp)
+dreamitemprice.append(shopp)
 money = int(input("You have money now?: "))
 print(money)
 moneygoat = int(input("You more money?: "))
 print(moneygoat)
-alprice = []
-alprice.append(shopp)
 money = money + moneygoat
 togoal = shopp - money
 print(abs(moneygoat),"goat money on day")
-if money == shop:
+if togoal >= -1:
+ print("To goal: ",int(togoal))
+else:
+ print("Yea.. you have a extra money: ", togoal)
+if money >= shopp:
  print("WHOO!!YOU ARE BUY NOW ITEM!!")
 else:
- print("To goal: ",int(togoal))
+ print("Wait more money...")
